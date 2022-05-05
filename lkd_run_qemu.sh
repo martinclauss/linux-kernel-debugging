4df3e1def3d19caebcb3ab3e59273445e63814dd
Martin Clauß <mc@cs.uni-bonn.de>
2022-05-05 16:38:36 +0200

    reduce memory to 4GB

--- lkd_run_qemu.sh
+++ lkd_run_qemu.sh
@@ -15,7 +15,7 @@ fi
 # qemu-system-x86_64 -kernel arch/x86_64/boot/bzImage -append "root=/dev/sda rw console=ttyS0 nokaslr" -drive file=./lkd_qemu_image.qcow2,format=raw --enable-kvm -cpu host --nographic -m 4096 -net nic,model=virtio -net user,hostfwd=tcp:127.0.0.1:2222-:22 -smp 1 $DEBUG |& tee lkd_vm.log
 
 # w/o KVM support
-qemu-system-x86_64 -kernel arch/x86_64/boot/bzImage -append "root=/dev/sda rw console=ttyS0 nokaslr" -drive file=./lkd_qemu_image.qcow2,format=raw --nographic -m 8192 -net nic,model=e1000 -net user,hostfwd=tcp:127.0.0.1:2222-:22 -smp 1 $DEBUG |& tee lkd_vm.log
+qemu-system-x86_64 -kernel arch/x86_64/boot/bzImage -append "root=/dev/sda rw console=ttyS0 nokaslr" -drive file=./lkd_qemu_image.qcow2,format=raw --nographic -m 4096 -net nic,model=e1000 -net user,hostfwd=tcp:127.0.0.1:2222-:22 -smp 1 $DEBUG |& tee lkd_vm.log
 
 # reset the terminal
 reset

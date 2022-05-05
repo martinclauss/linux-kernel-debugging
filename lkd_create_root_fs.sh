0122020a04e7241b81b3a2a378a2549b9fcd44b8
Martin Clauß <mc@cs.uni-bonn.de>
2022-05-05 23:05:55 +0200

    make execution more robust in case of errors

--- lkd_create_root_fs.sh
+++ lkd_create_root_fs.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-ROOT_PASSWD_HASH=$(openssl passwd -1 test)
-
 IMG=lkd_qemu_image.qcow2
 DIR=mount-point.dir
 
-qemu-img create $IMG 5g
-mkfs.ext2 $IMG
+ROOT_PASSWD_HASH=$(openssl passwd -1 test) && \
+qemu-img create $IMG 5g && \
+mkfs.ext2 $IMG && \
+mkdir $DIR && \
+sudo mount -o loop $IMG $DIR && \
+sudo debootstrap --arch amd64 bullseye $DIR && \
+sudo sed -i -e "s#root:\*#root:${ROOT_PASSWD_HASH}#" $DIR/etc/shadow && \
+echo "lkd-debian-qemu" | sudo tee $DIR/etc/hostname && \
+sudo umount $DIR && \
+rmdir $DIR && exit 0
 
-mkdir $DIR
-sudo mount -o loop $IMG $DIR
-sudo debootstrap --arch amd64 bullseye $DIR
-
-sudo sed -i -e "s#root:\*#root:${ROOT_PASSWD_HASH}#" $DIR/etc/shadow
-echo "lkd-debian-qemu" | sudo tee $DIR/etc/hostname
-
-sudo umount $DIR
-rmdir $DIR
+exit 1
